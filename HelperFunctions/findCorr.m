6ca4e90152d9734b36a4aef645eb27d67befe2bc
Dani Hak <dani.hak@gmail.com>
2018-03-29 13:11:07 -0400

    bubble filtering
    3 sigma exclustion for bubbles,
    didn't work,put a flag instead of temporal filtering. might be due to fixed pattern. check unwrapping image.

--- HelperFunctions/findCorr.m
+++ HelperFunctions/findCorr.m
@@ -1,7 +1,7 @@
 function [ callq,mt2star,sfs,tsnr,st2star] = findCorr( staticSlices, curPos,img,angDeg )
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
-CreateMapFlag=0;
+BubbleMapFlag=0;
 %goodSlices=10:26;
 %indStatic=timing(goodSlices)<1500;
 %indStatic=timing(goodSlices)<500 & timing(goodSlices) >0;
@@ -9,7 +9,7 @@ CreateMapFlag=0;
 boldActual=reverseSeq(curPos,[6000 7000],512*16);
 
 
-getStatBold3(img(:,:,floor(mean(staticSlices)),:),angDeg,1,3,CreateMapFlag,[-0.5 -0.5],pi*0.15);
+getStatBold3(img(:,:,floor(mean(staticSlices)),:),angDeg,1,3,BubbleMapFlag,[-0.5 -0.5],pi*0.15);
 
 % plot([boldExpected(:)  boldActual(:)])
 % legend('bold expected','bold actual')
@@ -22,7 +22,7 @@ getStatBold3(img(:,:,floor(mean(staticSlices)),:),angDeg,1,3,CreateMapFlag,[-0.5
 l=length(staticSlices);
 for ii=1:l
       % [mt2star(:,:,ii),innerCylinder, center(:,ii), radius(:,ii)] =getMeanBold(img,staticSlices(ii),angDeg,0,3,CreateMapFlag);
-      [mt2star(:,:,ii),sfs(ii,:) , tsnr(ii,:),st2star(ii,:)] =getStatBold3(img(:,:,staticSlices(ii),:),angDeg,0,3,CreateMapFlag,[-0.5 -0.5],pi*0.15);
+      [mt2star(:,:,ii),sfs(ii,:) , tsnr(ii,:),st2star(ii,:)] =getStatBold3(img(:,:,staticSlices(ii),:),angDeg,0,3,BubbleMapFlag,[-0.5 -0.5],pi*0.15);
 end
 
 
